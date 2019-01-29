90ac920de00f7d5ff60f9d52c25fec0f8fac306f
tim <tim@birtleyhouse.co.uk>
2019-01-29 21:44:23 +0000

    Update arrangement.js

--- arrangement.js
+++ arrangement.js
@@ -1,1 +1,1 @@
-document.write("<aside><details><summary>Arrangements</summary><p>Arrangements are as follows...</p></details></aside>")
+document.write("<aside><details><summary>Arrangements</summary><p><script src="aside-content.js"></script></p></details></aside>")
