337d2587f38ee5517f68dab4693eb1b6ee36243f
tim <tim@birtleyhouse.co.uk>
2019-01-31 22:41:22 +0000

    Update duty-rosta.js

--- organisation/duty-rosta.js
+++ organisation/duty-rosta.js
@@ -1,5 +1,5 @@
-document.getElementById("rgn-house").innerHTML = "Anca Herlo";
-document.getElementById("rgn-annexe").innerHTML = "Manisha Gurung";
+document.getElementById("rgn-house").innerHTML = "Senior RGN Mirela Simion";
+document.getElementById("rgn-annexe").innerHTML = "RGN Manisha Gurung";
 document.getElementById("care-supervisor").innerHTML = "Michaela Pollard";
 document.getElementById("senior-carer-house").innerHTML = "Anna Fulton";
 document.getElementById("senior-carer-annexe").innerHTML = "Barbara Bassett";
