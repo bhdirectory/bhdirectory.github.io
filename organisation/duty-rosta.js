f504d420658a398def1460465276863c81e604b8
tim <tim@birtleyhouse.co.uk>
2019-01-31 23:18:01 +0000

    Update duty-rosta.js

--- organisation/duty-rosta.js
+++ organisation/duty-rosta.js
@@ -1,4 +1,4 @@
-document.getElementById("duty-director").innerHTML = "<img src='/people/tim-whalley.jpg' height='85' width='50'><br /><p>Frank Whalley</p>";
+document.getElementById("duty-director").innerHTML = "<img src='/people/images/tim-whalley.jpg' height='85' width='50'><br /><p>Frank Whalley</p>";
 document.getElementById("rm-in").innerHTML = "IN";
 document.getElementById("rgn-house").innerHTML = "Mirela Simion, Senior RGN";
 document.getElementById("rgn-annexe").innerHTML = "Manisha Gurung, RGN";
