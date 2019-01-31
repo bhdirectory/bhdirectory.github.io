0d8ff671a75b5f7453103db638efdb570db065b0
tim <tim@birtleyhouse.co.uk>
2019-01-31 22:47:07 +0000

    rosta

--- organisation/duty-rosta.js
+++ organisation/duty-rosta.js
@@ -1,5 +1,7 @@
+document.getElementById("duty-director").innerHTML = "Frank Whalley";
 document.getElementById("rgn-house").innerHTML = "Senior RGN Mirela Simion";
 document.getElementById("rgn-annexe").innerHTML = "RGN Manisha Gurung";
 document.getElementById("care-supervisor").innerHTML = "Michaela Pollard";
 document.getElementById("senior-carer-house").innerHTML = "Anna Fulton";
 document.getElementById("senior-carer-annexe").innerHTML = "Barbara Bassett";
+ocument.getElementById("out-of-hours").innerHTML = "Tim Whalley";
