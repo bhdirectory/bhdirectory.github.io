a795226ccf5a995bed54a2e7a825c8d347f8f625
bhdirectory <42696143+bhdirectory@users.noreply.github.com>
2019-03-06 14:48:30 +0000

    loler

---
+++ asset/loler-register/update.js
@@ -0,0 +1,1 @@
+document.write("LOLER inspections are up-to-date.  Monthly checks have been devised and are scheduled to start in April 2019.  There are a number of slings that need re-labelling.  Senior Care Staff have been given additional duties to ensure that all manual handling equipment is safe and stored appropriately.");
