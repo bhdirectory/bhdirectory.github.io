03605ec1c2ae738bc99fccc890c1816accfea71f
bhdirectory <42696143+bhdirectory@users.noreply.github.com>
2019-03-06 15:39:51 +0000

    puwer

---
+++ asset/plant-register/update.js
@@ -0,0 +1,1 @@
+document.write("\<i\>06/03/2019\<\/i\> - PUWER inspections are up-to-date.  All equipment has been serviced according to the appropriate schedule and gas safety checks are up-to-date.");
