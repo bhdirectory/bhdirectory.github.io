e17f0f3b08293a528ad0c751e83e9f9354d2046b
tim <tim@birtleyhouse.co.uk>
2019-03-08 23:20:19 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 function myFunction() {
-  var x = document.getElementsByTagName('title')[0];
-  document.getElementById("test").innerHTML = x.innerHTML;
+  var x = document.getElementsByTagName('H3');
+  document.getElementById("test").innerHTML = x[0].innerHTML;
 }
