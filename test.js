ba98eedbefc149768bc94380d3088823dcf3854b
tim <tim@birtleyhouse.co.uk>
2019-03-09 22:54:58 +0000

    adding json link to make

--- test.js
+++ test.js
@@ -6,6 +6,10 @@ function myFunction() {
   var title = JSON.parse(document.querySelector('#datablock3').innerText);
   document.getElementById('title').innerText=title.headline;
   document.getElementById('headerTitle').innerText=title.headline;
+
+  var make = JSON.parse(document.querySelector('#datablock1').innerText);
+  document.getElementById('make').innerText=make.manufacturer.name;
+  
 }
 
 myFunction();
