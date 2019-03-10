0b7075e77b457d0fa790bcb4d5af5dfb1a6c5343
tim <tim@birtleyhouse.co.uk>
2019-03-10 23:01:18 +0000

    Update dataCoupler.js

--- dataCoupler.js
+++ dataCoupler.js
@@ -2,7 +2,7 @@
 - - - get data from json-ld and set in HTML - - -
 */
 
-function myFunction() {
+function dataCouple() {
   var coachOne = JSON.parse(document.querySelector('#coachOne').innerText);
   document.getElementById('assetId').innerText=coachOne.identifier.value;
   document.getElementById('make').innerText=coachOne.manufacturer.name;
@@ -28,4 +28,4 @@ function myFunction() {
   document.getElementById('headerTitle').innerText=coachThree.headline;
 }
 
-myFunction();
+dataCouple();
