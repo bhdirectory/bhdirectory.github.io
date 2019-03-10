2b424b16753be290c73d3c862a156a4f37aa30ee
tim <tim@birtleyhouse.co.uk>
2019-03-10 21:21:10 +0000

    coaches id

--- dataCoupler.js
+++ dataCoupler.js
@@ -3,30 +3,29 @@
 */
 
 function myFunction() {
-  var dbthree = JSON.parse(document.querySelector('#datablock3').innerText);
-  document.getElementById('title').innerText=dbthree.headline;
-  document.getElementById('headerTitle').innerText=dbthree.headline;
+  var coachOne = JSON.parse(document.querySelector('#coachOne').innerText);
+  document.getElementById('assetId').innerText=coachOne.identifier.value;
+  document.getElementById('make').innerText=coachOne.manufacturer.name;
+  document.getElementById('make').href=coachOne.manufacturer.url;
+  document.getElementById('model').innerText=coachOne.model.name;
+  document.getElementById('model').href=coachOne.model.url;
+  document.getElementById('sn').innerText=coachOne.serialNumber;
+  document.getElementById('location').innerText=coachOne.location.name;
+  document.getElementById('location').href=coachOne.location.url;
+  document.getElementById('condition').innerText=coachOne.itemCondition;
+  document.getElementById('mailtoPass').href="mailto:maintenance@birtleyhouse.co.uk?subject=Bed%20Check%20PASS%20" + coachOne.identifier.value + "&body=No%20action%20required";
+  document.getElementById('mailtoFail').href="mailto:maintenance@birtleyhouse.co.uk?subject=Bed%20Check%20FAIL%20" + coachOne.identifier.value + "&body=Reason%20for%20failure:&20";
 
-  var dbone = JSON.parse(document.querySelector('#datablock1').innerText);
-  document.getElementById('assetId').innerText=dbone.identifier.value;
-  document.getElementById('make').innerText=dbone.manufacturer.name;
-  document.getElementById('make').href=dbone.manufacturer.url;
-  document.getElementById('model').innerText=dbone.model.name;
-  document.getElementById('model').href=dbone.model.url;
-  document.getElementById('sn').innerText=dbone.serialNumber;
-  document.getElementById('location').innerText=dbone.location.name;
-  document.getElementById('location').href=dbone.location.url;
-  document.getElementById('condition').innerText=dbone.itemCondition;
-  document.getElementById('mailtoPass').href="mailto:maintenance@birtleyhouse.co.uk?subject=Bed%20Check%20PASS%20" + dbone.identifier.value + "&body=No%20action%20required";
-  document.getElementById('mailtoFail').href="mailto:maintenance@birtleyhouse.co.uk?subject=Bed%20Check%20FAIL%20" + dbone.identifier.value + "&body=Reason%20for%20failure:&20";
+  var coachTwo = JSON.parse(document.querySelector('#coachTwo').innerText);
+  document.getElementById('handsetMake').innerText=coachTwo.manufacturer.name;
+  document.getElementById('handsetMake').href=coachTwo.manufacturer.url;
+  document.getElementById('handsetModel').innerText=coachTwo.model.name;
+  document.getElementById('handsetModel').href=coachTwo.model.url;
+  document.getElementById('handsetCondition').innerText=coachTwo.itemCondition;
 
-  var dbtwo = JSON.parse(document.querySelector('#datablock2').innerText);
-  document.getElementById('handsetMake').innerText=dbtwo.manufacturer.name;
-  document.getElementById('handsetMake').href=dbtwo.manufacturer.url;
-  document.getElementById('handsetModel').innerText=dbtwo.model.name;
-  document.getElementById('handsetModel').href=dbtwo.model.url;
-  document.getElementById('handsetCondition').innerText=dbtwo.itemCondition;
-
+  var coachThree = JSON.parse(document.querySelector('#coachThree').innerText);
+  document.getElementById('title').innerText=coachThree.headline;
+  document.getElementById('headerTitle').innerText=coachThree.headline;
 }
 
 myFunction();
