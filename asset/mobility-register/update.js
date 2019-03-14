60401f3c70402d7d8e65b033ac3538c152436d3a
bhdirectory <42696143+bhdirectory@users.noreply.github.com>
2019-03-14 12:38:39 +0000

    wheelchairs

---
+++ asset/mobility-register/update.js
@@ -0,0 +1,1 @@
+document.write("\<i\>14/03/2019\<\/i\> - A register of all wheelchairs has been created and will be updated as part of the monthly check although this has not yet been embedded.  The intention is to start this from April.  A new agreement will be drawn up for all residents who own and use an electric-powered wheelchair on site.");
