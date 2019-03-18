64b1426d4c0afc3426274ad05a41c80f0dd7d354
tim <tim@birtleyhouse.co.uk>
2019-03-18 16:28:21 +0000

    Update request.js

--- asset/lift-register/request.js
+++ asset/lift-register/request.js
@@ -3,7 +3,7 @@ function loadXMLDoc() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-    document.getElementById('make').innerText=coachOne.manufacturer.name;
+    document.getElementById('index').innerText=coachOne.manufacturer.name;
     }
   };
   xhttp.open("GET", "3001.html", true);
