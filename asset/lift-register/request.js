ea846d8a0b11c28591c6a70d50de2ff3befbd9f4
tim <tim@birtleyhouse.co.uk>
2019-03-18 18:12:22 +0000

    Update request.js

--- asset/lift-register/request.js
+++ asset/lift-register/request.js
@@ -1,12 +1,12 @@
 function loadXMLDoc() {
   var xhttp = new XMLHttpRequest();
+  xhttp.open("GET", "3001.html", true);
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
     var coachThree = JSON.parse(document.querySelector('#coachThree').innerText);
     document.getElementById('index').innerText=coachThree.headline;
     }
   };
-  xhttp.open("GET", "3001.html", true);
   xhttp.send();
 }
 
