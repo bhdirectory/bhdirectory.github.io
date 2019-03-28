33279a62c0946bcb40e8e9a576316a7c96d0bfb5
tim <tim@birtleyhouse.co.uk>
2019-03-28 21:48:20 +0000

    contractor

--- contractor/dataCoupler.js
+++ contractor/dataCoupler.js
@@ -9,12 +9,12 @@ function dataCouple() {
   document.getElementById('contractorId').innerText=coaches.identifier.value;
   document.getElementById('companyName').innerText=coaches.provider.legalName;
   document.getElementById('service').innerText=coaches.serviceType;
-  document.getElementById('contactName').innerText=coaches.provider.contactPoint.name;
-  document.getElementById('mobile').innerText=coaches.provider.contactPoint.telephone;
+  document.getElementById('contactName').innerText=coaches.provider.employee.name;
+  document.getElementById('mobile').innerText=coaches.provider.employee.telephone;
   document.getElementById('telephone').innerText=coaches.provider.telephone;
   document.getElementById('website').innerText=coaches.provider.url;
   document.getElementById('email').innerText=coaches.provider.email;
-  document.getElementById('jobTitle').innerText=coaches.provider.contactPoint.jobTitle;
+  document.getElementById('jobTitle').innerText=coaches.provider.employee.jobTitle;
 }
 
 dataCouple();
