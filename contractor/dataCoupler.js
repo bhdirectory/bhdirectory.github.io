c6598650d8014804334c04e63b9f56f6d13f522e
bhdirectory <42696143+bhdirectory@users.noreply.github.com>
2019-03-29 12:23:57 +0000

    contractor

--- contractor/dataCoupler.js
+++ contractor/dataCoupler.js
@@ -13,10 +13,14 @@ function dataCouple() {
   document.getElementById('description').innerText=coaches.description;
   document.getElementById('contactName').innerText=coaches.provider.employee.name;
   document.getElementById('jobTitle').innerText=coaches.provider.employee.jobTitle;
+  document.getElementById('telephone').href="tel:" + coaches.provider.telephone;
   document.getElementById('telephone').innerText=coaches.provider.telephone;
+  document.getElementById('mobile').href="tel:" + coaches.provider.employee.telephone;
   document.getElementById('mobile').innerText=coaches.provider.employee.telephone;
   document.getElementById('email').innerText=coaches.provider.email;
   document.getElementById('email').href="mailto:" + coaches.provider.email + "?subject=Birtley House%20|%20";
+  document.getElementById('termsOfService').innerText=coaches.termsOfService.name;
+  document.getElementById('termsOfService').href=coaches.termsOfService.url;
 
 }
 
