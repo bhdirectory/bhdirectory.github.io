425cf5693b439532105c99f1ac7ad72d9093d50e
bhdirectory <42696143+bhdirectory@users.noreply.github.com>
2019-04-08 13:11:44 +0100

    Update dataCoupler.js

--- check/hs-audit/dataCoupler.js
+++ check/hs-audit/dataCoupler.js
@@ -3,7 +3,7 @@
 */
 
 function dataCouple() {
-  var coachThree = JSON.parse(document.querySelector('#coaches').innerText);
+  var coaches = JSON.parse(document.querySelector('#coaches').innerText);
   document.title=coaches.headline;
   document.getElementById('headerTitle').innerText=coaches.headline;
 }
