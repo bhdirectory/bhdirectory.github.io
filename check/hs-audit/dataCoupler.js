548b420197a46fd48225eb03f6be95316705fa77
bhdirectory <42696143+bhdirectory@users.noreply.github.com>
2019-04-08 13:02:03 +0100

    audit index

---
+++ check/hs-audit/dataCoupler.js
@@ -0,0 +1,11 @@
+/*
+- - - get data from json-ld and set in HTML - - -
+*/
+
+function dataCouple() {
+  var coachThree = JSON.parse(document.querySelector('#coaches').innerText);
+  document.title=coaches.headline;
+  document.getElementById('headerTitle').innerText=coachThree.headline;
+}
+
+dataCouple();
---
+++ check/hs-audit/update.js
@@ -0,0 +1,1 @@
+document.write("\<i\>08/04/2019\<\/i\> - The next Audit is due in May.");
