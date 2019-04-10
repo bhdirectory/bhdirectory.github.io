e4dd4794932a5a9107dc5751c2ba6b9cbdb8310a
tim <tim@birtleyhouse.co.uk>
2019-04-10 21:28:41 +0100

    Update dataCoupler.js

--- asset/loler-register/dataCoupler.js
+++ asset/loler-register/dataCoupler.js
@@ -3,22 +3,21 @@
 */
 
 function dataCouple() {
-  var coachOne = JSON.parse(document.querySelector('#coachOne').innerText);
-  document.getElementById('assetId').innerText=coachOne.identifier.value;
-  document.getElementById('make').innerText=coachOne.manufacturer.name;
-  document.getElementById('make').href=coachOne.manufacturer.url;
-  document.getElementById('model').innerText=coachOne.model.name;
-  document.getElementById('model').href=coachOne.model.url;
-  document.getElementById('sn').innerText=coachOne.serialNumber;
-  document.getElementById('location').innerText=coachOne.location.name;
-  document.getElementById('location').href=coachOne.location.url;
-  document.getElementById('condition').innerText=coachOne.itemCondition;
-  document.getElementById('mailtoPass').href="mailto:maintenance@birtleyhouse.co.uk?subject=Hoist%20Check%20PASS%20" + coachOne.identifier.value + " | " + coachOne.location.name + "&body=No%20action%20required";
-  document.getElementById('mailtoFail').href="mailto:maintenance@birtleyhouse.co.uk?subject=Hoist%20Check%20FAIL%20" + coachOne.identifier.value + " | " + coachOne.location.name + "&body=Reason%20for%20failure:&20";
+  var coaches = JSON.parse(document.querySelector('#coaches').innerText);
+  document.title=coaches.mainEntityOfPage.headline;
+  document.getElementById('headerTitle').innerText=coaches.mainEntityOfPage.headline;
+  document.getElementById('assetId').innerText=coaches.identifier.value;
+  document.getElementById('make').innerText=coaches.manufacturer.name;
+  document.getElementById('make').href=coaches.manufacturer.url;
+  document.getElementById('model').innerText=coaches.model.name;
+  document.getElementById('model').href=coaches.model.url;
+  document.getElementById('sn').innerText=coaches.serialNumber;
+  document.getElementById('location').innerText=coaches.location.name;
+  document.getElementById('location').href=coaches.location.url;
+  document.getElementById('condition').innerText=coaches.itemCondition;
+  document.getElementById('mailtoPass').href="mailto:maintenance@birtleyhouse.co.uk?subject=Hoist%20Check%20PASS%20" + coaches.identifier.value + " | " + coaches.location.name + "&body=No%20action%20required";
+  document.getElementById('mailtoFail').href="mailto:maintenance@birtleyhouse.co.uk?subject=Hoist%20Check%20FAIL%20" + coaches.identifier.value + " | " + coaches.location.name + "&body=Reason%20for%20failure:&20";
 
-  var coachThree = JSON.parse(document.querySelector('#coachThree').innerText);
-  document.title=coachThree.headline;
-  document.getElementById('headerTitle').innerText=coachThree.headline;
 }
 
 dataCouple();
