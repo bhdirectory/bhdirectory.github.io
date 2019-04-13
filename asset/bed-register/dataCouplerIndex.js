af5e263447d133234f8b22642353fb1cc0307fbf
tim <tim@birtleyhouse.co.uk>
2019-04-13 23:33:11 +0100

    bed register

---
+++ asset/bed-register/dataCouplerIndex.js
@@ -0,0 +1,13 @@
+/*
+- - - get data from json-ld and set in HTML - - -
+*/
+
+function dataCouple() {
+  var coaches = JSON.parse(document.querySelector('#coaches').innerText);
+  document.title=coaches.mainEntityOfPage.headline;
+  document.getElementById('headerTitle').innerText=coaches.mainEntityOfPage.headline;
+  document.getElementById('one').innerText=coaches.itemListElement[1].position;
+  document.getElementById('two').innerText=coaches.itemListElement[2].position;
+}
+
+dataCouple();
