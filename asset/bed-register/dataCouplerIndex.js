0cb1090566b7bf3b2c18b2479f434b0327952fc1
tim <tim@birtleyhouse.co.uk>
2019-04-13 23:38:39 +0100

    Update dataCouplerIndex.js

--- asset/bed-register/dataCouplerIndex.js
+++ asset/bed-register/dataCouplerIndex.js
@@ -6,8 +6,8 @@ function dataCouple() {
   var coaches = JSON.parse(document.querySelector('#coaches').innerText);
   document.title=coaches.mainEntityOfPage.headline;
   document.getElementById('headerTitle').innerText=coaches.mainEntityOfPage.headline;
-  document.getElementById('one').innerText=coaches.itemListElement[0].position;
-  document.getElementById('two').innerText=coaches.itemListElement[1].position;
+  document.getElementById('one').innerText=coaches.itemListElement.position;
+  document.getElementById('two').innerText=coaches.itemListElement[0].position;
 }
 
 dataCouple();
