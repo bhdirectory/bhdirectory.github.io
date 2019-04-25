ebd24821557ce60b9e794ba9db93d0fa15a7111d
tim <tim@birtleyhouse.co.uk>
2019-04-25 23:24:20 +0100

    loler records

--- asset/loler-register/dataCoupler.js
+++ asset/loler-register/dataCoupler.js
@@ -6,7 +6,7 @@ function dataCouple() {
   var coaches = JSON.parse(document.querySelector('#coaches').innerText);
   document.title=coaches.mainEntityOfPage.headline;
   document.getElementById('headerTitle').innerText=coaches.mainEntityOfPage.headline;
-  document.getElementById('assetId').innerText=coaches.identifier.value;
+  document.getElementById('assetId').innerText=coaches.identifier;
   document.getElementById('make').innerText=coaches.manufacturer.name;
   document.getElementById('make').href=coaches.manufacturer.url;
   document.getElementById('model').innerText=coaches.model.name;
@@ -14,9 +14,14 @@ function dataCouple() {
   document.getElementById('sn').innerText=coaches.serialNumber;
   document.getElementById('location').innerText=coaches.location.name;
   document.getElementById('location').href=coaches.location.url;
+  document.getElementById('date').innerText=coaches.purchaseDate;
   document.getElementById('condition').innerText=coaches.itemCondition;
   document.getElementById('mailtoPass').href="mailto:maintenance@birtleyhouse.co.uk?subject=Hoist%20Check%20PASS%20" + coaches.identifier.value + " | " + coaches.location.name + "&body=No%20action%20required";
   document.getElementById('mailtoFail').href="mailto:maintenance@birtleyhouse.co.uk?subject=Hoist%20Check%20FAIL%20" + coaches.identifier.value + " | " + coaches.location.name + "&body=Reason%20for%20failure:&20";
+  document.getElementById('service').innerText=coaches.isRelatedTo[0].provider.name;
+  document.getElementById('service').href=coaches.isRelatedTo[0].provider.url;
+  document.getElementById('insurance').innerText=coaches.isRelatedTo[1].provider.name;
+  document.getElementById('insurance').href=coaches.isRelatedTo[1].provider.url;
 
 }
 
