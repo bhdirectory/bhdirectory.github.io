8ec322848fb0bad60fbb64cf3f859808b2b8b3a9
tim <tim@birtleyhouse.co.uk>
2019-05-17 03:36:54 +0100

    epb index updates

--- asset/asbestos-register/update.js
+++
@@ -1,1 +0,0 @@
-document.write("\<i\>15/03/2019\<\/i\> - An annual review of Asbestos in the building is due.  HunterFM are arranging a date with a specialist third-party.");
--- asset/bed-register/dataCouplerIndex.js
+++ asset/bed-register/dataCouplerIndex.js
@@ -6,6 +6,9 @@ function dataCouple() {
   var coaches = JSON.parse(document.querySelector('#coaches').innerText);
   document.title=coaches.mainEntityOfPage.headline;
   document.getElementById('headerTitle').innerText=coaches.mainEntityOfPage.headline;
+  document.getElementById('up0').innerText=coaches.hasPart[0].datePublished + " - " + coaches.hasPart[0].text;
+  document.getElementById('up1').innerText=coaches.hasPart[1].datePublished + " - " + coaches.hasPart[1].text;
+  document.getElementById('up2').innerText=coaches.hasPart[2].datePublished + " - " + coaches.hasPart[2].text;
   document.getElementById('p0').innerText=coaches.itemListElement[0].position;
   document.getElementById('a0').innerText=coaches.itemListElement[0].item.name;
   document.getElementById('a0').href=coaches.itemListElement[0].item.url;
