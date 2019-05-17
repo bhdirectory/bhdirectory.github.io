4589cbbe6266181b69d54d5c4a354781263cc827
tim <tim@birtleyhouse.co.uk>
2019-05-17 04:48:04 +0100

    Update dataCouplerIndex.js

--- asset/fire-register/dataCouplerIndex.js
+++ asset/fire-register/dataCouplerIndex.js
@@ -8,7 +8,7 @@ function dataCouple() {
   document.getElementById('headerTitle').innerText=coaches.mainEntityOfPage.headline;
   document.getElementById('up0').innerText=coaches.mainEntityOfPage.hasPart[0].datePublished + " - " + coaches.mainEntityOfPage.hasPart[0].text;
   document.getElementById('up1').innerText=coaches.mainEntityOfPage.hasPart[1].datePublished + " - " + coaches.mainEntityOfPage.hasPart[1].text;
-  document.getElementById('up2').innerText=coaches.mainEntityOfPage.hasPart[2].datePublished + " - " + coaches.mainEntityOfPage.hasPart[2].text;
+  /* document.getElementById('up2').innerText=coaches.mainEntityOfPage.hasPart[2].datePublished + " - " + coaches.mainEntityOfPage.hasPart[2].text; */
   document.getElementById('p0').innerText=coaches.itemListElement[0].item.name;
   document.getElementById('p0').href=coaches.itemListElement[0].item.url;
   document.getElementById('p1').innerText=coaches.itemListElement[1].item.name;
