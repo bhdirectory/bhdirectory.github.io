f8a30305f8800d59475b14059d3a866ac419acdf
tim <tim@birtleyhouse.co.uk>
2019-05-17 04:45:24 +0100

    fire coupler

--- asset/fire-register/dataCouplerIndex.js
+++ asset/fire-register/dataCouplerIndex.js
@@ -9,6 +9,16 @@ function dataCouple() {
   document.getElementById('up0').innerText=coaches.mainEntityOfPage.hasPart[0].datePublished + " - " + coaches.mainEntityOfPage.hasPart[0].text;
   document.getElementById('up1').innerText=coaches.mainEntityOfPage.hasPart[1].datePublished + " - " + coaches.mainEntityOfPage.hasPart[1].text;
   document.getElementById('up2').innerText=coaches.mainEntityOfPage.hasPart[2].datePublished + " - " + coaches.mainEntityOfPage.hasPart[2].text;
+  document.getElementById('p0').innerText=coaches.itemListElement[0].item.name;
+  document.getElementById('p0').href=coaches.itemListElement[0].item.url;
+  document.getElementById('p1').innerText=coaches.itemListElement[1].item.name;
+  document.getElementById('p1').href=coaches.itemListElement[1].item.url;
+  document.getElementById('p2').innerText=coaches.itemListElement[2].item.name;
+  document.getElementById('p2').href=coaches.itemListElement[2].item.url;
+  document.getElementById('p3').innerText=coaches.itemListElement[3].item.name;
+  document.getElementById('p3').href=coaches.itemListElement[3].item.url;
+  document.getElementById('p4').innerText=coaches.itemListElement[4].item.name;
+  document.getElementById('p4').href=coaches.itemListElement[4].item.url;
 }
 
 dataCouple();
