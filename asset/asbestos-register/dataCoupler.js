dd5e81a799f1958ebf153e42ee9f3cc529a64097
tim <tim@birtleyhouse.co.uk>
2019-05-17 03:21:57 +0100

    asbestos update

--- asset/asbestos-register/dataCoupler.js
+++ asset/asbestos-register/dataCoupler.js
@@ -6,8 +6,8 @@ function dataCouple() {
   var coaches = JSON.parse(document.querySelector('#coaches').innerText);
   document.title=coaches.headline;
   document.getElementById('headerTitle').innerText=coaches.headline;
-  document.getElementById('up0').innerText="Latest" + coaches.hasPart[0].about + ": " + coaches.hasPart[0].datePublished;
-  document.getElementById('uptxt0').innerText=coaches.hasPart[0].text;
+  document.getElementById('up0').innerText="Latest " + coaches.hasPart[0].about + ": ";
+  document.getElementById('uptxt0').innerText=coaches.hasPart[0].datePublished + " " + coaches.hasPart[0].text;
 }
 
 dataCouple();
