531bd9ccf36fd3996eec563b7d4b507187bdf210
tim <tim@birtleyhouse.co.uk>
2019-05-17 03:29:00 +0100

    Update dataCoupler.js

--- asset/asbestos-register/dataCoupler.js
+++ asset/asbestos-register/dataCoupler.js
@@ -6,9 +6,9 @@ function dataCouple() {
   var coaches = JSON.parse(document.querySelector('#coaches').innerText);
   document.title=coaches.headline;
   document.getElementById('headerTitle').innerText=coaches.headline;
-  document.getElementById('up0').innerText=coaches.hasPart[0].datePublished + " " + coaches.hasPart[0].text;
-  document.getElementById('up1').innerText=coaches.hasPart[1].datePublished + " " + coaches.hasPart[1].text;
-  document.getElementById('up2').innerText=coaches.hasPart[2].datePublished + " " + coaches.hasPart[2].text;
+  document.getElementById('up0').innerText=coaches.hasPart[0].datePublished + " - " + coaches.hasPart[0].text;
+  document.getElementById('up1').innerText=coaches.hasPart[1].datePublished + " - " + coaches.hasPart[1].text;
+  document.getElementById('up2').innerText=coaches.hasPart[2].datePublished + " - " + coaches.hasPart[2].text;
 }
 
 dataCouple();
