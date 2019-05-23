c2561b9c3e9ebf8b3abe9323d1694d2dadf65bd9
tim <tim@birtleyhouse.co.uk>
2019-05-23 21:44:38 +0100

    index coupler

--- asset/mobility-register/dataCouplerIndex.js
+++ asset/mobility-register/dataCouplerIndex.js
@@ -5,6 +5,8 @@
 function dataCouple() {
   var coaches = JSON.parse(document.querySelector('#coaches').innerText);
   document.title=coaches.mainEntityOfPage.headline;
+  document.getElementById('up0').innerText=coaches.mainEntityOfPage.hasPart[0].datePublished + " - " + coaches.mainEntityOfPage.hasPart[0].text;
+  document.getElementById('up1').innerText=coaches.mainEntityOfPage.hasPart[1].datePublished + " - " + coaches.mainEntityOfPage.hasPart[1].text;
   document.getElementById('headerTitle').innerText=coaches.mainEntityOfPage.headline;
   document.getElementById('p0').innerText=coaches.itemListElement[0].position;
   document.getElementById('a0').innerText=coaches.itemListElement[0].item.name;
