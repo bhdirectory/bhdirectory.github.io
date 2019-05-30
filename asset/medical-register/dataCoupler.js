447f2c6e03103f2b4c92f5c0d08a53ea31c55dc1
tim <tim@birtleyhouse.co.uk>
2019-05-30 22:35:23 +0100

    med reg

--- asset/medical-register/dataCoupler.js
+++ asset/medical-register/dataCoupler.js
@@ -4,8 +4,7 @@
 
 function dataCouple() {
   var coaches = JSON.parse(document.querySelector('#coaches').innerText);
-  document.title=coaches.headline;
-  document.getElementById('headerTitle').innerText=coaches.headline;
+  document.title=coaches.mainEntityOfPage.headline;
   document.getElementById('headerTitle').innerText=coaches.mainEntityOfPage.headline;
   document.getElementById('assetId').innerText=coaches.identifier;
   document.getElementById('make').innerText=coaches.manufacturer.name;
@@ -17,8 +16,8 @@ function dataCouple() {
   document.getElementById('condition').innerText=coaches.itemCondition;
   document.getElementById('mailtoPass').href="mailto:maintenance@birtleyhouse.co.uk?subject=Medica%20Device%20Check%20PASS%20" + coaches.identifier + " | " + "&body=No%20action%20required";
   document.getElementById('mailtoFail').href="mailto:maintenance@birtleyhouse.co.uk?subject=Medica%20Device%20Check%20FAIL%20" + coaches.identifier + " | " + "&body=Reason%20for%20failure:&20";
-  document.getElementById('service').innerText=coaches.isRelatedTo[0].provider.name;
-  document.getElementById('service').href=coaches.isRelatedTo[0].provider.url;
+  document.getElementById('service').innerText=coaches.isRelatedTo.provider.name;
+  document.getElementById('service').href=coaches.isRelatedTo.provider.url;
   document.getElementById('inspection').innerText=coaches.subjectOf.validFrom;
   document.getElementById('inspection').href=coaches.subjectOf.url;
   document.getElementById('inspectionDue').innerText=coaches.subjectOf.validUntil;
