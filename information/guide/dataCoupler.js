ea054238df86e029eeeb1e5fc65899fe97087b79
tim <tim@birtleyhouse.co.uk>
2019-06-08 22:47:26 +0100

    data coupler breadcrumb

--- information/guide/dataCoupler.js
+++ information/guide/dataCoupler.js
@@ -5,6 +5,10 @@
 function dataCouple() {
   var coaches = JSON.parse(document.querySelector('#coaches').innerText);
   document.title=coaches.headline;
+  document.getElementById('home').innerText=coaches.isPartOf.name;
+  document.getElementById('home').href=coaches.isPartOf.url;
+  document.getElementById('breadcrumb').innerText=coaches.breadcrumb.name;
+  document.getElementById('breadcrumb').href=coaches.breadcrumb.url;
   document.getElementById('headerTitle').innerText=coaches.headline;
 }
 
