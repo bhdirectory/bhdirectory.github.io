d5683d80a8d1929effdd020e7a3ff5787d1eeb99
tim <tim@birtleyhouse.co.uk>
2019-06-08 21:27:57 +0100

    guide services

---
+++ information/dataCoupler.js
@@ -0,0 +1,14 @@
+/*
+- - - get data from json-ld and set in HTML - - -
+*/
+
+function dataCouple() {
+  var coaches = JSON.parse(document.querySelector('#coaches').innerText);
+  document.title=coaches.headline;
+  document.getElementById('headerTitle').innerText=coaches.headline;
+  document.getElementById('up0').innerText=coaches.hasPart[0].text;
+  document.getElementById('up1').innerText=coaches.hasPart[1].text;
+  document.getElementById('up2').innerText=coaches.hasPart[2].text;
+}
+
+dataCouple();
