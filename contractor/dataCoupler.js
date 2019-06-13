3f6123abbdd883361fcc12a0cb26bb5aa20e4acd
tim <tim@birtleyhouse.co.uk>
2019-06-13 22:58:08 +0100

    Update dataCoupler.js

--- contractor/dataCoupler.js
+++ contractor/dataCoupler.js
@@ -5,7 +5,7 @@
 function dataCouple() {
   var coaches = JSON.parse(document.querySelector('#coaches').innerText);
   document.title=coaches.provider.legalName + " | " + coaches.category;
-  document.getElementById('headerTitle').innerText=coaches.provider.legalName + " | " + coaches.category;
+  document.getElementById('headerTitle').innerText=coaches.provider.legalName;
   document.getElementById('companyNumber').innerText=coaches.provider.identifier.value;
   document.getElementById('companyNumber').href=coaches.provider.identifier.url;
   document.getElementById('website').innerText=coaches.provider.url;
