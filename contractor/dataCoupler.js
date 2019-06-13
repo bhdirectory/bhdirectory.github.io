3f3e64d3fe58e79ccb6c144978d40e64ff801b13
tim <tim@birtleyhouse.co.uk>
2019-06-13 22:55:57 +0100

    contractor

--- contractor/dataCoupler.js
+++ contractor/dataCoupler.js
@@ -10,22 +10,16 @@ function dataCouple() {
   document.getElementById('companyNumber').href=coaches.provider.identifier.url;
   document.getElementById('website').innerText=coaches.provider.url;
   document.getElementById('website').href=coaches.provider.url;
+  document.getElementById('telephone').href="tel:" + coaches.provider.telephone;
+  document.getElementById('telephone').innerText=coaches.provider.telephone;
   document.getElementById('service').innerText=coaches.category;
   document.getElementById('termsOfService').innerText=coaches.termsOfService.name;
   document.getElementById('termsOfService').href=coaches.termsOfService.url;
   document.getElementById('description').innerText=coaches.description;
-  document.getElementById('contactName').innerText=coaches.provider.employee[0].name;
-  document.getElementById('jobTitle').innerText=coaches.provider.employee[0].jobTitle;
-  document.getElementById('contactNameAlt').innerText=coaches.provider.employee[1].name;
-  document.getElementById('jobTitleAlt').innerText=coaches.provider.employee[1].jobTitle;
-  document.getElementById('mobile').href="tel:" + coaches.provider.employee[0].telephone;
-  document.getElementById('mobile').innerText=coaches.provider.employee[0].telephone;
-  document.getElementById('mobileAlt').href="tel:" + coaches.provider.employee[1].telephone;
-  document.getElementById('mobileAlt').innerText=coaches.provider.employee[1].telephone;
-  document.getElementById('email').innerText=coaches.provider.employee[0].email;
-  document.getElementById('email').href="mailto:" + coaches.provider.employee[0].email + "?subject=Birtley House%20|%20";
-  document.getElementById('emailAlt').innerText=coaches.provider.employee[1].email;
-  document.getElementById('emailAlt').href="mailto:" + coaches.provider.employee[1].email + "?subject=Birtley House%20|%20";
+  document.getElementById('contactName').innerText=coaches.provider.employee.name;
+  document.getElementById('jobTitle').innerText=coaches.provider.employee.jobTitle;
+  document.getElementById('email').innerText=coaches.provider.employee.email;
+  document.getElementById('email').href="mailto:" + coaches.provider.employee.email + "?subject=Birtley House%20|%20";
   document.getElementById('telephone').href="tel:" + coaches.provider.telephone;
   document.getElementById('telephone').innerText=coaches.provider.telephone;
 
