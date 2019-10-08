1cc06107b8272f2888dc0767804ea755222a8739
tim <tim@birtleyhouse.co.uk>
2019-10-08 21:58:25 +0100

    Update header.js

--- header.js
+++ header.js
@@ -1,1 +1,1 @@
-document.write("<h3 id='headerTitle'></h3><a href='./' class='fa fa-bars' style='font-size: 1.5em;'>  SECTION INDEX</a><a href='/' class='fa fa-bars' style='font-size: 1.5em;'>  DIRECTORY HOME</a>")
+document.write("<a href='./' class='fa fa-bars' style='font-size: 1.5em;'>  SECTION INDEX</a><a href='/' class='fa fa-bars' style='font-size: 1.5em;'>  DIRECTORY HOME</a><h3 id='headerTitle'></h3>")
