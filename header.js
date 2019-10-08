da9c5559f20810edf2fcdfded36b66885f3190d0
tim <tim@birtleyhouse.co.uk>
2019-10-08 21:56:17 +0100

    Update header.js

--- header.js
+++ header.js
@@ -1,1 +1,1 @@
-document.write("<img src='https://bhdirectory.github.io/birtleyhouse-white.jpg' width='100' height='85'/><a href='./' class='fa fa-bars' style='font-size: 1.5em;'>  SECTION INDEX</a><a href='/' class='fa fa-bars' style='font-size: 1.5em;'>  DIRECTORY HOME</a><h3 id='headerTitle'></h3>")
+document.write("<h3 id='headerTitle'></h3><a href='./' class='fa fa-bars' style='font-size: 1.5em;'>  SECTION INDEX</a><a href='/' class='fa fa-bars' style='font-size: 1.5em;'>  DIRECTORY HOME</a>")
