e7684db8083b79b3350c98cab7c922495c04434b
tim <tim@birtleyhouse.co.uk>
2019-10-09 22:34:02 +0100

    guide to services header trial breadcrumb

--- header-trial.js
+++ header-trial.js
@@ -1,1 +1,1 @@
-document.write("<img src='https://bhdirectory.github.io/bh-logo.png' width='100' height='100'/><a class='breadcrumb' href='#' id='home'></a> > <a class='breadcrumb' href='#' id='breadcrumb'></a><h3 id='headerTitle'></h3>")
+document.write("<img src='https://bhdirectory.github.io/bh-logo.png' width='100' height='100'/><a class='breadcrumb' href='#' id='h0'><span> > </span></a><a class='breadcrumb' href='#' id='h1'></a><span> > </span><a class='breadcrumb' href='#' id='h3'></a><h3 id='headerTitle'></h3>")
--- service/guide/dataCouplerIndex.js
+++ service/guide/dataCouplerIndex.js
@@ -5,6 +5,12 @@
 function dataCouple() {
   var coaches = JSON.parse(document.querySelector('#coaches').innerText);
   document.title=coaches.mainEntityOfPage.headline;
+  document.getElementById('h0').innerText=coaches.breadcrumb.itemListElement[0].item.name;
+  document.getElementById('h0').href=coaches.breadcrumb.itemListElement[0].item.url;
+  document.getElementById('h1').innerText=coaches.breadcrumb.itemListElement[0].item.name;
+  document.getElementById('h1').href=coaches.breadcrumb.itemListElement[0].item.url;
+  document.getElementById('h2').innerText=coaches.breadcrumb.itemListElement[0].item.name;
+  document.getElementById('h2').href=coaches.breadcrumb.itemListElement[0].item.url;
   document.getElementById('headerTitle').innerText=coaches.mainEntityOfPage.headline;
   document.getElementById('a0').innerText=coaches.itemListElement[0].item.name;
   document.getElementById('a0').href=coaches.itemListElement[0].item.url;
