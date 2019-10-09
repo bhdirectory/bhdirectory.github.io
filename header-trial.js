575b32047b25d8dd384491f7acbd1999ddf4cb83
tim <tim@birtleyhouse.co.uk>
2019-10-09 22:12:22 +0100

    class for header trial

--- header-trial.js
+++ header-trial.js
@@ -1,1 +1,1 @@
-document.write("<img src='https://bhdirectory.github.io/bh-logo.png' width='100' height='100'/><a href='#' id='home'></a> > <a href='#' id='breadcrumb'></a><h3 id='headerTitle'></h3>")
+document.write("<img src='https://bhdirectory.github.io/bh-logo.png' width='100' height='100'/><a class='breadcrumb' href='#' id='home'></a> > <a class='breadcrumb' href='#' id='breadcrumb'></a><h3 id='headerTitle'></h3>")
