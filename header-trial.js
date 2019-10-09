3c4b1b99691baf22f7876126e7ed2132a1cee525
tim <tim@birtleyhouse.co.uk>
2019-10-09 23:01:22 +0100

    Update header-trial.js

--- header-trial.js
+++ header-trial.js
@@ -1,1 +1,1 @@
-document.write("<img src='https://bhdirectory.github.io/bh-logo.png' width='100' height='100'/><a class='breadcrumb' href='#' id='h0'></a><a class='breadcrumb' href='#' id='h1'></a><a class='breadcrumb' href='#' id='h2'></a><h3 id='headerTitle'></h3>")
+document.write("<img src='https://bhdirectory.github.io/bh-logo.png' width='100' height='100'/><a class='breadcrumb' href='#' id='h0'></a><span> >> </span><a class='breadcrumb' href='#' id='h1'></a><span> >> </span><a class='breadcrumb' href='#' id='h2'></a><h3 id='headerTitle'></h3>")
