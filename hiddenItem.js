7558a1e3c05575de788335d1f3cc5f2b8183abb7
tim <tim@birtleyhouse.co.uk>
2019-10-28 21:27:55 +0000

    hiddenItem

---
+++ hiddenItem.js
@@ -0,0 +1,14 @@
+/*
+- - - hide/show text in hidden-item id - - -
+*/
+
+function displayFunction() {
+  document.getElementById("hidden-item").style.display = "grid";
+}
+
+function hideFunction() {
+  document.getElementById("hidden-item").style.display = "none";
+}
+
+displayFunction();
+hideFunction();
