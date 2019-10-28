8778a5a46f2be4c655a9ffb4cd0803edc857d71f
tim <tim@birtleyhouse.co.uk>
2019-10-28 21:47:14 +0000

    content to text

--- asset/lift-register/dataCouplerIndex.js
+++ asset/lift-register/dataCouplerIndex.js
@@ -6,7 +6,7 @@ function dataCouple() {
   var coaches = JSON.parse(document.querySelector('#coaches').innerText);
   document.title=coaches.mainEntityOfPage.headline;
   document.getElementById('headerTitle').innerText=coaches.mainEntityOfPage.headline;
-  document.getElementById('hidden-item').innerText=coaches.mainEntityOfPage.content;
+  document.getElementById('hidden-item').innerText=coaches.mainEntityOfPage.text;
   document.getElementById('a0').innerText=coaches.itemListElement[0].item.name;
   document.getElementById('a0').href=coaches.itemListElement[0].item.url;
   document.getElementById('a1').innerText=coaches.itemListElement[1].item.name;
