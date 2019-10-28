7e2051dcb7eda040581af60c3d4929ebbcea71b5
tim <tim@birtleyhouse.co.uk>
2019-10-28 22:02:05 +0000

    working at height

---
+++ asset/ladder-register/dataCouplerIndex.js
@@ -0,0 +1,18 @@
+/*
+- - - get data from json-ld and set in HTML - - -
+*/
+
+function dataCouple() {
+  var coaches = JSON.parse(document.querySelector('#coaches').innerText);
+  document.title=coaches.mainEntityOfPage.headline;
+  document.getElementById('headerTitle').innerText=coaches.mainEntityOfPage.headline;
+  document.getElementById('hidden-item').innerText=coaches.mainEntityOfPage.text;
+  document.getElementById('a0').innerText=coaches.itemListElement[0].item.name;
+  document.getElementById('a0').href=coaches.itemListElement[0].item.url;
+  document.getElementById('a1').innerText=coaches.itemListElement[1].item.name;
+  document.getElementById('a1').href=coaches.itemListElement[1].item.url;
+  document.getElementById('a2').innerText=coaches.itemListElement[2].item.name;
+  document.getElementById('a2').href=coaches.itemListElement[2].item.url;
+}
+
+dataCouple();
--- asset/ladder-register/update.js
+++
@@ -1,1 +0,0 @@
-document.write("\<i\>15/03/2019\<\/i\> - A review of the condition of all ladders and step-ladders is overdue.  This is scheduled for March.  All training is up-to-date.  The current high-level work in the Main Hall has a RAMS in place and the contractor concerned has relevant insurance.");
