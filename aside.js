83c5b4b4af74ff8dbc0804e040611c190af4e883
tim <tim@birtleyhouse.co.uk>
2019-11-03 09:41:03 +0000

    Update aside.js

--- aside.js
+++ aside.js
@@ -1,1 +1,1 @@
-document.write("<button onclick='toggleFunction()'>Safety Arrangements</button><div id='toggle-item'></div><a class='aside-item' href='qa/'>Quality Assurance</a>")
+document.write("<div class='linkButton' onclick='toggleFunction()'>Safety Arrangements</button><div id='toggle-item'></div><a class='aside-item' href='qa/'>Quality Assurance</a>")
