7d5c30fcf33259003abf7ad9205eac13cb43a7a4
tim <tim@birtleyhouse.co.uk>
2020-01-06 22:51:12 +0000

    portable devices index

---
+++ asset/it-register/portable-devices/dataCoupler.js
@@ -0,0 +1,21 @@
+/*
+- - - get data from json-ld and set in HTML - - -
+*/
+
+function dataCouple() {
+  var coaches = JSON.parse(document.querySelector('#coaches').innerText);
+  document.title=coaches.mainEntityOfPage.headline;
+  document.getElementById('headerTitle').innerText=coaches.mainEntityOfPage.headline;
+  document.getElementById('assetId').innerText=coaches.identifier;
+  document.getElementById('make').innerText=coaches.manufacturer.name;
+  document.getElementById('make').href=coaches.manufacturer.url;
+  document.getElementById('model').innerText=coaches.model.name;
+  document.getElementById('model').href=coaches.model.url;
+  document.getElementById('sn').innerText=coaches.serialNumber;
+  document.getElementById('condition').innerText=coaches.itemCondition;
+  document.getElementById('department').innerText=coaches.isRelatedTo.identifier.description;
+  document.getElementById('extension').innerText=coaches.isRelatedTo.identifier.value;
+  document.getElementById('extension').href=coaches.isRelatedTo.identifier.url;
+}
+
+dataCouple();
