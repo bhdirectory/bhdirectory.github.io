eab211647a36a2aa3eb2b1ef2bf04cd5e9d1b0c8
tim <tim@birtleyhouse.co.uk>
2021-06-18 21:29:00 +0100

    deletions

--- organisation/duty-rosta.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("duty-director").innerHTML = "<figure><img src='/people/images/tim-whalley.jpg' alt='tim-whalley.jpg' height='70' width='60'><figcaption><h3>Tim Whalley</h3></figcaption></figure>";
-document.getElementById("rm-in").innerHTML = "IN";
-document.getElementById("rgn-house").innerHTML = "Mirela Simion, Senior RGN";
-document.getElementById("rgn-annexe").innerHTML = "Manisha Gurung, RGN";
-document.getElementById("care-supervisor").innerHTML = "Michaela Pollard";
-document.getElementById("senior-carer-house").innerHTML = "Anna Fulton";
-document.getElementById("senior-carer-annexe").innerHTML = "Barbara Bassett";
-document.getElementById("out-of-hours").innerHTML = "Tim Whalley";
